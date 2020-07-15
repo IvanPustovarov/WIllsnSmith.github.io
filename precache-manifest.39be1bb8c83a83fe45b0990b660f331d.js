9f675bc3447e368a4f3f1ae328cae6f2085b626a
Winston Smith <silverflash150@gmail.com>
2020-07-15 14:23:34 +0300

    Updates

---
+++ precache-manifest.39be1bb8c83a83fe45b0990b660f331d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d277a1eded251a698380c55d8e4182a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ccea30b9fb583ef3c00",
+    "url": "/static/css/main.18dba3b2.chunk.css"
+  },
+  {
+    "revision": "b96ba30361ec99dbbd65",
+    "url": "/static/js/2.4332045a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4332045a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ccea30b9fb583ef3c00",
+    "url": "/static/js/main.735203b0.chunk.js"
+  },
+  {
+    "revision": "8cedc8e8064a91556169",
+    "url": "/static/js/runtime-main.36799215.js"
+  },
+  {
+    "revision": "d8be28502d60c2f31c21a62befe1ac94",
+    "url": "/static/media/calendar.d8be2850.svg"
+  },
+  {
+    "revision": "c3f85a1357ece0c83fa3b351fca880c3",
+    "url": "/static/media/check.c3f85a13.svg"
+  },
+  {
+    "revision": "b71f11f4cb00742c74fe487ec9c9f969",
+    "url": "/static/media/checkAfter.b71f11f4.svg"
+  },
+  {
+    "revision": "1b31c6ff636c73df9b994352645adc1f",
+    "url": "/static/media/dot.1b31c6ff.svg"
+  },
+  {
+    "revision": "c5f7cecd4d72abd22566d9ce609133a0",
+    "url": "/static/media/next.c5f7cecd.svg"
+  },
+  {
+    "revision": "be4de7d65872bd29b43406c1be2d954d",
+    "url": "/static/media/prev.be4de7d6.svg"
+  }
+]);
